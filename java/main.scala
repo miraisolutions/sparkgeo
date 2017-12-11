7b9ca22e197e0e50895e8244ae99e3726a126581
Martin Studer <martin.remo.studer@gmail.com>
2017-12-11 12:33:47 +0100

    Handling null in `within` UDF

--- java/main.scala
+++ java/main.scala
@@ -10,7 +10,11 @@ object UDF {
     magellan.Utils.injectRules(spark)
 
     spark.udf.register("point", (latitude: Double, longitude: Double) => Point(longitude, latitude))
-    spark.udf.register("within", (point: Point, polygon: Polygon) => point.within(polygon))
+    spark.udf.register("within", (point: Point, polygon: Polygon) => (point, polygon) match {
+      case (null, _) => None
+      case (_, null) => None
+      case (poi, pol) => Some(poi.within(pol))
+    })
     spark.udf.register("metadata_string", (metadata: Map[String, Any], name: String) =>
       metadata(name).asInstanceOf[String])
   }
