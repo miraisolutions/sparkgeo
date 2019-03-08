fab5043e83439f1dfc7bedcb0116a03b509adebf
Roland Schmid <roland.schmid@mirai-solutions.com>
2019-03-08 18:04:38 +0100

    add warning for unsupported versions

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,11 +1,17 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
+  jars = system.file(
+    sprintf("java/sparkgeo-%s-%s.jar", spark_version, scala_version),
+    package = "sparkgeo"
+  )
+  if (jars == "")
+    # we can still allow spark_connect() to continue, but sparkgeo_register(sc) will fail later
+    warning(sprintf(
+      "sparkgeo extension: unsupported version combination - spark: %s, scala: %s",
+      spark_version,
+      scala_version
+    ))
   sparklyr::spark_dependency(
-    jars = c(
-      system.file(
-        sprintf("java/sparkgeo-%s-%s.jar", spark_version, scala_version),
-        package = "sparkgeo"
-      )
-    ),
+    jars = jars,
     packages = sprintf("harsha2010:magellan:1.0.5-s_%s", scala_version)
   )
 }
